c6929fba5a8c6406689b7b45bb82dd3f64eb5854
Faheetah <Faheetah@users.noreply.github.com>
2021-09-19 07:58:23 -0500

    Add functionality to parse entire hex dumps for valid packets

--- lib/libvirt/rpc/hex.ex
+++ lib/libvirt/rpc/hex.ex
@@ -5,8 +5,10 @@ defmodule Libvirt.RPC.Hex2Dec do
 
   @space 0x0020
 
-  defparsec :parse_hex,
-    eventually(ignore(utf8_char([?:])))
+  parse_hex_block =
+    ignore(string("        0x"))
+    |> ignore(integer(4))
+    |> ignore(string(":  "))
     |> lookahead_not(utf8_char([?\n]))
     |> repeat(choice([
       ignore(utf8_char([@space])),
@@ -14,7 +16,76 @@ defmodule Libvirt.RPC.Hex2Dec do
     ])
     )
     |> eventually(ignore(utf8_char([?\n])))
+
+  defparsec :parse_hex,
+    parse_hex_block
     |> repeat()
+
+  defparsec :parse_dump,
+    ignore(utf8_string([?0..?9, ?:, ?.], 15))
+    |> ignore(utf8_char([@space]))
+    |> ignore(string("IP"))
+    |> eventually(ignore(string("length ")))
+    |> unwrap_and_tag(integer(min: 1), :length)
+    |> eventually(ignore(utf8_char([?\n])))
+    |> unwrap_and_tag(eventually(utf8_string([?0..?9, ?.], min: 8)), :source_ip)
+    |> ignore(utf8_char([@space]))
+    |> ignore(utf8_char([?>]))
+    |> ignore(utf8_char([@space]))
+    |> unwrap_and_tag(eventually(utf8_string([?0..?9, ?.], min: 8)), :dest_ip)
+    |> eventually(ignore(utf8_char([?\n])))
+    |> tag(repeat(parse_hex_block), :hex)
+    |> wrap()
+    |> repeat()
+
+  def dump_to_raw(dump) do
+    {:ok, parsed, "", _, _, _} = __MODULE__.parse_dump(dump)
+    parsed
+  end
+
+  def hexdump_to_raw(hex) do
+    {:ok, parsed, "", _, _, _} = __MODULE__.parse_hex(hex)
+    parsed
+  end
+
+  def dump_to_packets(data, filter_invalid_packets \\ false) do
+    data
+    |> dump_to_raw()
+    |> Enum.with_index()
+    |> Enum.map(fn {dump, i} ->
+      case parsed_hex_to_packet(dump[:hex]) do
+        {:ok, packet} -> Keyword.put(dump, :hex, packet)
+        {:error, _} -> dump
+      end
+      |> Keyword.put(:dest_ip, parse_address(dump[:dest_ip]))
+      |> Keyword.put(:source_ip, parse_address(dump[:source_ip]))
+      |> Keyword.put(:packet, i + 1)
+    end)
+    |> Enum.reject(fn x -> is_list(x[:hex]) and filter_invalid_packets end)
+  end
+
+  def parse_address(address) do
+    [ip1, ip2, ip3, ip4, port] = String.split(address, ".")
+    {port, _} = Integer.parse(port)
+    [ip: Enum.join([ip1, ip2, ip3, ip4], "."), port: port]
+  end
+
+  def hexdump_to_packet(hex) do
+    {:ok, parsed, "", _, _, _} = __MODULE__.parse_hex(hex)
+    parsed_hex_to_packet(parsed)
+  end
+
+  def parsed_hex_to_packet(parsed) do
+    parsed
+    |> Enum.map(fn p ->
+      p
+      |> to_charlist
+      |> List.to_integer(16)
+    end)
+    |> Enum.drop(52)
+    |> Enum.into(<<>>, fn bit -> <<bit::8>> end)
+    |> Libvirt.RPC.Packet.decode
+  end
 
   # this is admittedly pretty bad, but it is for debugging Libvirt RPC calls
   def hexdump_to_decimal(hex, as_string \\ false) do
