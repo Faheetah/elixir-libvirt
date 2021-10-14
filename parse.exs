9caacb0769ba3e7921284c97e204d914077f1037
Faheetah <Faheetah@users.noreply.github.com>
2021-10-14 13:47:35 -0500

    Use dumps folder

--- parse.exs
+++ parse.exs
@@ -1,7 +1,7 @@
 alias Libvirt.RPC.Hex2Dec
 
-lib_dump = File.read!("elixir-hostname.full.dump")
+lib_dump = File.read!("dumps/elixir-hostname.full.dump")
 IO.inspect Hex2Dec.dump_to_packets(lib_dump, true)
 
-cli_dump = File.read!("hostname.full.dump")
+cli_dump = File.read!("dumps/hostname.full.dump")
 IO.inspect Hex2Dec.dump_to_packets(cli_dump, true)
