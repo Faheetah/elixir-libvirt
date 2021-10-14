fdb1ff3ecff67179a0ca532946de128a33476e31
Faheetah <Faheetah@users.noreply.github.com>
2021-10-14 16:40:40 -0500

    Clean up basic_test

--- basic_test.exs
+++ basic_test.exs
@@ -1,11 +1,8 @@
 IO.puts "connecting"
 {:ok, socket} = Libvirt.Hypervisor.connect("colosseum.sudov.im")
 
-File.rm_rf!("images/")
-File.mkdir_p!("images/")
-
 Libvirt.RPC.Call.connect_get_hostname(socket)
-|> IO.inspect
+|> IO.inspect(label: :'Libvirt.RPC.Call.connect_get_hostname')
 
 Libvirt.Network.list_all(socket)
-|> IO.inspect(label: :network2)
+|> IO.inspect(label: :'Libvirt.Network.list_all')
