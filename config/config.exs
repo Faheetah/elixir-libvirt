4a532bd8d7202656dafbe8794e39f718b64a93e9
Faheetah <Faheetah@users.noreply.github.com>
2021-10-17 18:49:52 -0500

    Set default as direct

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,6 @@ import Config
 config :logger,
   backends: [:console]
 
-config :libvirt, :rpc, backend: Libvirt.RPC.Backends.Shared
+config :libvirt, :rpc, backend: Libvirt.RPC.Backends.Direct
 
 import_config "#{config_env()}.exs"
