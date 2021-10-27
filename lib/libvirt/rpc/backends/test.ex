904e97ae073fe514f07ac5e64e08ddffc4b90549
Faheetah <Faheetah@users.noreply.github.com>
2021-10-27 14:06:44 -0500

    Better handling around test connection for errors

--- lib/libvirt/rpc/backends/test.ex
+++ lib/libvirt/rpc/backends/test.ex
@@ -1,11 +1,28 @@
 defmodule Libvirt.RPC.Backends.Test do
+  @moduledoc """
+  This should be turned into something akin to the Shared type so connections
+  can be left open and refreshed
+  """
+
   def connect!(host) do
     {:ok, socket} = connect(host)
     socket
   end
 
   def connect(host) do
-    Libvirt.RPC.Backends.Direct.connect(host, "test:///default", 57087)
+    socket = Process.get(:socket)
+    if socket do
+      {:ok, socket}
+    else
+      case Libvirt.RPC.Backends.Direct.connect(host, "test:///default", 57087) do
+        {:ok, socket} ->
+          Process.put(:socket, socket)
+          {:ok, socket}
+
+        error ->
+          error
+      end
+    end
   end
 
   defdelegate send(socket, packet, type), to: Libvirt.RPC.Backends.Direct
